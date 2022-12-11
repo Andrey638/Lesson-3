e6fc7ede764db5012adb3cb827ef1909207f3abe
Andrey3544 <box4435@mail.ru>
2022-12-11 14:26:50 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,24 @@
+﻿//принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.WriteLine("введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+if(number < 10000 || number > 99999)
+{
+ Console.WriteLine("не корректное число");
+}
+else
+{
+    int pal = 0, num = number; 
+        while (number > 0)
+        {
+            pal = pal * 10 + number % 10; number /= 10;
+        } 
+    if(num==pal)
+    {
+        Console.WriteLine("палиндром");
+    }
+    else
+    {
+      Console.WriteLine("не палиндром");  
+    }
+}
